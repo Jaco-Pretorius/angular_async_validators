246743dc52e6589d7c447d5aafd26fc68d0b0837
Jaco Pretorius <jdjpretorius@gmail.com>
2015-03-08 14:29:51 -0400

    Enable cross-origin queries

--- application.rb
+++ application.rb
@@ -1,10 +1,16 @@
 require "sinatra"
+require 'sinatra/cross_origin'
 require "json"
+
+configure do
+  enable :cross_origin
+end
 
 get "/usernames" do
   query = params["q"]
+  capitalizedQuery = query && query.capitalize
   matches = ["Bob", "Steve", "James", "Dave"].select do |username|
-    /#{username}/i =~ query
+    username == capitalizedQuery
   end
 
   matches.to_json
